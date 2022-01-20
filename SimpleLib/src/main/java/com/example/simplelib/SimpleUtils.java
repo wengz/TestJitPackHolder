35b0d1d31a3e1247dbf0537dce84f8ffd2deca16
zhicheng.weng <zhicheng.weng@tpv-tech.com>
2022-01-20 17:17:58 +0800

    init

---
+++ SimpleLib/src/main/java/com/example/simplelib/SimpleUtils.java
@@ -0,0 +1,8 @@
+package com.example.simplelib;
+
+public class SimpleUtils {
+
+    public static String getStr() {
+        return "2021-01-20";
+    }
+}
---
+++ app/src/main/java/com/example/testjitpackholder/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testjitpackholder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.simplelib.SimpleUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv_hello);
+        String newContent = tv.getText()+ SimpleUtils.getStr();
+        tv.setText(newContent);
+    }
+}
